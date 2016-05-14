5bfc676ab0d5580cfdb82c17b146e2d2203382c0
Hazard <Hazard@noreply.com>
2016-05-14 21:43:54 +0100

    Adding endpoint for retrieving status for all servers.

--- src/IsKronosUpYet.API/Controllers/StatusController.cs
+++ src/IsKronosUpYet.API/Controllers/StatusController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using IsKronosUpYet.API.Models;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
 using Newtonsoft.Json;
 
 namespace IsKronosUpYet.API.Controllers
@@ -18,7 +20,53 @@ namespace IsKronosUpYet.API.Controllers
         {
             _context = context;
         }
-        
+
+        /// <summary>
+        /// This method is used to check the server status for aall tracked servers 
+        /// </summary>
+        /// <returns>JSON representation of the server status for every server</returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var statuses = _context.ServerStatus
+                .Include(st => st.Server)
+                .Where(s => s.Timestamp > (DateTimeOffset.UtcNow - TimeSpan.FromMinutes(15)))
+                .ToList();
+
+            var returnObject = statuses.GroupBy(ss => ss.Server.Id)
+                .Select(g =>
+                {
+                    var latestStatus = g.Where(st => st.Server.Id == g.Key)
+                        .OrderByDescending(st => st.Timestamp)
+                        .FirstOrDefault();
+
+                    if (latestStatus == null)
+                    {
+                        // TODO: Log this as a real error
+                        return new
+                        {
+                            Id = g.Key,
+                            Up = false,
+                            LastUpdated = DateTimeOffset.MinValue,
+                            Error = "No status report within the last 15 minutes for this server."
+                        };
+                    }
+
+                    return new
+                    {
+                        Id = g.Key,
+                        Up = latestStatus.Status,
+                        LastUpdated = latestStatus.Timestamp,
+                        Error = ""
+                    };
+                })
+                .ToList();
+
+         
+            var serialized = JsonConvert.SerializeObject(returnObject);
+            return this.Ok(serialized);
+        }
+
         /// <summary>
         /// This method is used to check the server status for a server (by id)
         /// </summary>
